96cdf7bdde948f73e5cc5260fe64ab8ddb997702
iBamboola <41113234+iBamboola@users.noreply.github.com>
2021-05-11 11:47:48 +0300

    Initial Commit

---
+++ SpacePhoto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Алексей Сергеев on 11.05.2021.
+//  Copyright © 2021 Алексей Сергеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
